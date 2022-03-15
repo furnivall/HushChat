a158fd1a11cb7db58cfe8c97e14019655b276c86
furnivall <danielfurnivall@gmail.com>
2022-03-15 23:25:13 +0000

    hardcoded port

--- HushChat/app/src/main/java/com/example/hushchat/SocketHandler.kt
+++ HushChat/app/src/main/java/com/example/hushchat/SocketHandler.kt
@@ -1,5 +1,6 @@
 package com.example.hushchat
 
+import android.util.Log
 import io.socket.client.IO
 import io.socket.client.Socket
 import java.net.URISyntaxException
@@ -10,9 +11,11 @@ object SocketHandler {
     @Synchronized
     fun setSocket() {
         try{
-            mSocket = IO.socket("http://10.0.2.2:8080")
-        }catch (e: URISyntaxException) {
+            mSocket = IO.socket("http://20.126.74.125:8080")
+//            mSocket = IO.socket("http://10.0.2.2:8080")
+        }catch (e: Exception) {
 
+            Log.e("e", "Error connecting to server.")
         }
     }
 
